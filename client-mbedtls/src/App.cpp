f2e666e6df0f330e4a09231341a198ff3a2048c3
lganzzzo <lganzzzo@gmail.com>
2019-06-09 13:37:20 +0300

    client-mbedtls example added

---
+++ client-mbedtls/src/App.cpp
@@ -0,0 +1,64 @@
+
+#include "WSListener.hpp"
+
+#include "oatpp-websocket/WebSocket.hpp"
+#include "oatpp-websocket/Connector.hpp"
+
+#include "oatpp-mbedtls/client/ConnectionProvider.hpp"
+#include "oatpp-mbedtls/Config.hpp"
+
+#include <thread>
+
+namespace {
+
+  bool finished = false;
+
+  void socketTask(const std::shared_ptr<oatpp::websocket::WebSocket>& websocket) {
+    websocket->listen();
+    OATPP_LOGD("websocket-client", "SOCKET CLOSED!!!");
+    finished = true;
+  }
+
+}
+
+void run() {
+
+  OATPP_LOGI("websocket-client", "Application Started");
+
+  auto config = oatpp::mbedtls::Config::createDefaultClientConfigShared();
+
+  auto connectionProvider = oatpp::mbedtls::client::ConnectionProvider::createShared(config, "echo.websocket.org", 443);
+
+  auto connector = oatpp::websocket::Connector::createShared(connectionProvider);
+
+  auto connection = connector->connect("/");
+
+  OATPP_LOGI("websocket-client", "Connected");
+
+  auto socket = oatpp::websocket::WebSocket::createShared(connection, true /* maskOutgoingMessages must be true for clients */);
+
+  std::mutex socketWriteMutex;
+
+  socket->setListener(std::make_shared<WSListener>(socketWriteMutex));
+
+  std::thread thread(socketTask, socket);
+
+  while(!finished) {
+    {
+      OATPP_LOGD("websocket-client", "sending message...");
+      std::lock_guard<std::mutex> lock(socketWriteMutex);
+      socket->sendOneFrameText("hello");
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
+  thread.join();
+
+}
+
+int main() {
+  oatpp::base::Environment::init();
+  run();
+  oatpp::base::Environment::destroy();
+  return 0;
+}
---
+++ client-mbedtls/src/WSListener.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Leonid  on 2019-06-07.
+//
+
+#include "WSListener.hpp"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// WSListener
+
+void WSListener::onPing(const WebSocket& socket, const oatpp::String& message) {
+  OATPP_LOGD(TAG, "onPing");
+  std::lock_guard<std::mutex> lock(m_writeMutex);
+  socket.sendPong(message);
+}
+
+void WSListener::onPong(const WebSocket& socket, const oatpp::String& message) {
+  OATPP_LOGD(TAG, "onPong");
+}
+
+void WSListener::onClose(const WebSocket& socket, v_word16 code, const oatpp::String& message) {
+  OATPP_LOGD(TAG, "onClose code=%d", code);
+}
+
+void WSListener::readMessage(const WebSocket& socket, p_char8 data, oatpp::data::v_io_size size) {
+
+  if(size == 0) { // message transfer finished
+
+    auto wholeMessage = m_messageBuffer.toString();
+    m_messageBuffer.clear();
+
+    OATPP_LOGD(TAG, "on message received '%s'", wholeMessage->c_str());
+
+    /* Send message in reply */
+    //std::lock_guard<std::mutex> lock(m_writeMutex);
+    //socket.sendOneFrameText( "Hello from oatpp!: " + wholeMessage);
+
+  } else if(size > 0) { // message frame received
+    m_messageBuffer.write(data, size);
+  }
+
+}
