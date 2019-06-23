a77224f5b9586b81ad10d14e0fcf002d132208a5
lganzzzo <lganzzzo@gmail.com>
2019-06-23 14:45:40 +0300

    setup CI

---
+++ ci-install-mbedtls.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf tmp
+
+mkdir tmp
+cd tmp
+
+git clone -b 'mbedtls-2.16.1' --single-branch --depth 1 --recurse-submodules https://github.com/ARMmbed/mbedtls
+
+cd mbedtls
+mkdir build && cd build
+
+cmake ..
+make
+make test
+make install
+
+cd ../
