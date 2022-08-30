53dca49e2764c152c0215c786cc0459a08795003
Leonid Stryzhevskyi <lganzzzo@gmail.com>
2022-08-31 01:45:47 +0300

    update API key in client example

--- client/src/App.cpp
+++ client/src/App.cpp
@@ -30,7 +30,7 @@ void run() {
 
   auto connector = oatpp::websocket::Connector::createShared(connectionProvider);
 
-  auto connection = connector->connect("v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self");
+  auto connection = connector->connect("v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self");
 
   OATPP_LOGI(TAG, "Connected");
 
